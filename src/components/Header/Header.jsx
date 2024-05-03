a85d65dab330a5940e60e2667d4db56b6703b141
usaid-anxari <usaidax@gmail.com>
2024-05-03 22:27:27 +0500

    Deploy

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,21 +1,24 @@
 import React from "react";
 import "./Header.css";
+import header_img from "../../assets/header_img.png";
 
 const Header = () => {
-  const goMenu =()=>{
-      <a href="explore-menu"></a>
-  }
   return (
     <div className="header">
-      <div className="header-content">
-        <h2>Order Your Favourite Food Here</h2>
-        <p>
-          Choose from a diverse menu featuring a delectable array of dishes
-          crafted with the finest ingredients and culinary expertise. Our
-          mission is to satisfy your carvings elevate your dinig experince, one
-          delicious meal at a time.
-        </p>
-        <button><a href="#explore-menu" >View Menu</a></button>  
+      <div >
+        <img className="header-logo" src={header_img} alt="" />
+        <div className="header-content">
+          <h2>Order Your Favourite Food Here</h2>
+          <p>
+            Choose from a diverse menu featuring a delectable array of dishes
+            crafted with the finest ingredients and culinary expertise. Our
+            mission is to satisfy your carvings elevate your dinig experince,
+            one delicious meal at a time.
+          </p>
+          <button>
+            <a href="#explore-menu">View Menu</a>
+          </button>
+        </div>
       </div>
     </div>
   );
